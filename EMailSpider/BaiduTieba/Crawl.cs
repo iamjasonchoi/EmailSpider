e48fd9d2b166f661faba5481d96d3f62b2150190
袁老三 <yhonglai@163.com>
2019-04-23 16:57:41 +0800

    test user

---
+++ EMailSpider/BaiduTieba/Crawl.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EMailSpider.BaiduTieba
+{
+    public class Crawl
+    {
+        public static HashSet<string> GetEMails(string url)
+        {
+            HashSet<string> emails = new HashSet<string>();
+
+
+            return emails;
+        }
+    }
+}
